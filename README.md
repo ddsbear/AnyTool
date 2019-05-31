b8eef79b69f0f1bccccc5bf495aa05ddebdc1782
dds <android_shuai@163.com>
2019-05-31 11:26:58 +0800

    permission

--- README.md
+++ README.md
@@ -7,5 +7,19 @@ Here is a record of the common problems encountered in Android development and h
 
 ## Records
 
-1. 
+1. permission
+
+   ```java
+    Permissions.request(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, integer -> {
+                   if (integer == PackageManager.PERMISSION_GRANTED) {
+                       VToast.show(MainActivity.this, "success accept : " + integer);
+                   } else {
+                       VToast.show(MainActivity.this, "failed accept : " + integer);
+                   }
+   
+   
+               });
+   ```
+
+   
 
--- app/src/main/java/com/trustmobi/dddemo/MainActivity.java
+++ app/src/main/java/com/trustmobi/dddemo/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.trustmobi.library.Permissions;
-import com.trustmobi.library.utils.Consumer;
 import com.trustmobi.library.utils.VToast;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,17 +23,14 @@ public class MainActivity extends AppCompatActivity {
     // Permission
     public void onPermission(View view) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            Permissions.request(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, new Consumer<Integer>() {
-                @Override
-                public void accept(Integer integer) {
-                    if (integer == PackageManager.PERMISSION_GRANTED) {
-                        VToast.show(MainActivity.this, "成功accept : " + integer);
-                    } else {
-                        VToast.show(MainActivity.this, "失败accept : " + integer);
-                    }
-
-
+            Permissions.request(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, integer -> {
+                if (integer == PackageManager.PERMISSION_GRANTED) {
+                    VToast.show(MainActivity.this, "成功accept : " + integer);
+                } else {
+                    VToast.show(MainActivity.this, "失败accept : " + integer);
                 }
+
+
             });
         }
 
