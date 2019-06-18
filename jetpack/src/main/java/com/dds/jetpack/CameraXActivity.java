9e337d854e39d0a266e9558161c46bb702d422b2
dds <android_shuai@163.com>
2019-06-18 10:53:37 +0800

    add JetPack——room

--- jetpack/src/main/java/com/dds/jetpack/CameraXActivity.java
+++ jetpack/src/main/java/com/dds/jetpack/CameraXActivity.java
@@ -3,31 +3,21 @@ package com.dds.jetpack;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class JetPackActivity extends AppCompatActivity {
+public class CameraXActivity extends AppCompatActivity {
 
     public static void openActivity(Activity activity) {
-        Intent intent = new Intent(activity, JetPackActivity.class);
+        Intent intent = new Intent(activity, CameraXActivity.class);
         activity.startActivity(intent);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jet_pack);
+        setContentView(R.layout.activity_camera_x);
     }
 
-    public void onRoom(View view) {
-        RoomActivity.openActivity(this);
 
-
-    }
-
-    public void onCamera(View view) {
-        CameraXActivity.openActivity(this);
-
-    }
 }
