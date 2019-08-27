cda58d552ca22b1165ec7abb59c4aad6e4225f01
dds <android_shuai@163.com>
2019-08-27 16:06:10 +0800

    okhttp3

--- any1_library/src/main/java/com/utils/library/net/okhttp/OkHttpRequest.java
+++ any1_library/src/main/java/com/utils/library/net/okhttp/OkHttpRequest.java
@@ -3,7 +3,9 @@ package com.utils.library.net.okhttp;
 
 import com.utils.library.net.HttpRequest;
 import com.utils.library.net.ICallback;
+import okhttp3.*;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -12,14 +14,56 @@ import java.util.Map;
 
 public class OkHttpRequest implements HttpRequest {
 
+    private OkHttpClient client = new OkHttpClient();
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     @Override
     public void get(String url, Map<String, String> params, ICallback callback) {
-
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                ResponseBody body = response.body();
+                if (body != null) {
+                    String result = body.string();
+                    callback.onSuccess(result);
+                }
+            } else {
+                callback.onFailure(response.code(), new Throwable(response.message()));
+            }
+        } catch (IOException e) {
+            callback.onFailure(-1, new Throwable("io error! please check your network"));
+        }
     }
 
     @Override
     public void post(String url, Map<String, String> params, ICallback callback) {
+        MultipartBody.Builder builder = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM);
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String mapKey = entry.getKey();
+            String mapValue = entry.getValue();
+            builder.addFormDataPart(mapKey, mapValue);
+        }
+        RequestBody requestBody = builder.build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                ResponseBody body = response.body();
+                if (body != null) {
+                    String result = body.string();
+                    callback.onSuccess(result);
+                }
+            }
+        } catch (IOException e) {
+            callback.onFailure(-1, new Throwable("io error! please check your network"));
+        }
 
     }
 }
