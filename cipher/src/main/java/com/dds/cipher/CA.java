32b4fe2bf34d16943992606f1f8ed0cf6412bd68
dds <android_shuai@163.com>
2019-09-14 23:55:25 +0800

    AES整理

--- cipher/src/main/java/com/dds/cipher/CA.java
+++ cipher/src/main/java/com/dds/cipher/CA.java
@@ -17,149 +17,20 @@ import org.bouncycastle.util.io.pem.PemObject;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.StringWriter;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Security;
-import java.security.Signature;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
-import javax.crypto.Cipher;
-
 public class CA {
-
-    private static final String publicKeyFileName = System.getProperty("user.dir") + File.separator + "pubkey.cer";
-    private static final String privateKeyFileName = System.getProperty("user.dir") + File.separator + "private.pfx";
-    private static final String pfxPassword = "123";//私钥文件获取时设置的密钥
-    private static String aliasName = "003";//alias名称
-
-    public CA() {
-        Security.addProvider(new BouncyCastleProvider());
-    }
-
-
-    // 簽名
-    public static byte[] sign(String str) {
-        byte[] base64Sign = null;
-        InputStream fis = null;
-        try {
-            fis = new FileInputStream(privateKeyFileName);
-            KeyStore keyStore = KeyStore.getInstance("PKCS12");
-            char[] pscs = pfxPassword.toCharArray();
-            keyStore.load(fis, pscs);
-            PrivateKey priKey = (PrivateKey) (keyStore.getKey(aliasName, pscs));
-            // 签名
-            Signature sign = Signature.getInstance("SHA256withRSA");
-            sign.initSign(priKey);
-            byte[] bysData = str.getBytes("UTF-8");
-            sign.update(bysData);
-            byte[] signByte = sign.sign();
-            base64Sign = Base64.encode(signByte);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return base64Sign;
-    }
-
-    // 驗證
-    public static boolean verify(String signStr, String verStr)
-            throws Exception {
-        boolean verfy = false;
-        InputStream fis = null;
-        try {
-            fis = new FileInputStream(publicKeyFileName);
-            CertificateFactory cf = CertificateFactory.getInstance("x509");
-            Certificate cerCert = cf.generateCertificate(fis);
-            PublicKey pubKey = cerCert.getPublicKey();
-            String signed = Base64.decode(signStr);
-            Signature sign = Signature.getInstance("SHA256withRSA");
-            sign.initVerify(pubKey);
-            sign.update(verStr.getBytes("UTF-8"));
-            verfy = sign.verify(signed.getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return verfy;
-    }
-
-    // 加密
-    public static byte[] encrypt(String source) throws Exception {
-        InputStream fis = null;
-        try {
-            fis = new FileInputStream(publicKeyFileName);
-            CertificateFactory cf = CertificateFactory.getInstance("x509");
-            Certificate cerCert = cf.generateCertificate(fis);
-            PublicKey pubKey = cerCert.getPublicKey();
-            Cipher cipher = Cipher.getInstance("RSA");
-            cipher.init(Cipher.ENCRYPT_MODE, pubKey);
-            byte[] sbt = source.getBytes();
-            byte[] epByte = cipher.doFinal(sbt);
-            byte[] epStr;
-            epStr = Base64.encode(epByte, 1);
-
-            return epStr;
-        } finally {
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    // 解密
-    public static String decode(String source) throws Exception {
-        String keyByte = Base64.decode(source);
-        InputStream fis = null;
-        try {
-            fis = new FileInputStream(privateKeyFileName);
-            KeyStore keyStore = KeyStore.getInstance("PKCS12");
-            char[] pscs = pfxPassword.toCharArray();
-            keyStore.load(fis, pscs);
-            PrivateKey priKey = (PrivateKey) (keyStore.getKey(aliasName, pscs));
-            Cipher cipher = Cipher.getInstance("RSA");
-            cipher.init(Cipher.DECRYPT_MODE, priKey);
-            byte[] epByte = cipher.doFinal(keyByte.getBytes());
-            return new String(epByte, "UTF-8");
-        } finally {
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
 
     // 生成KeyPair
     public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
@@ -197,7 +68,6 @@ public class CA {
         // base64便于网络传输
         return Base64.encode(str.toString());
     }
-
 
     public static void write(String content) throws IOException {
         //创建一个带缓冲区的输出流
